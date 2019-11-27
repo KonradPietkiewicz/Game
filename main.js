2c0fe02971ebcb6597c74d1aced0f3e653c275df
Konrad Pietkiewicz <konradpietkiewicz20@gmail.com>
2019-11-27 18:56:38 +0100

    Add  publishResult

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const gameSummary = {
     numbers: 0,
     wins: 0,
     losses: 0,
-    deaws: 0,
+    draws: 0,
 }
 
 const game = {
@@ -13,7 +13,7 @@ const game = {
 const hands = [...document.querySelectorAll('.select img')];
 
 function handSelection() {
-    console.log(this)
+    //console.log(this)
     game.playerHand = this.dataset.option;
     hands.forEach(hand => hand.style.boxShadow = "");
     this.style.boxShadow = "0 0 0 3px blue"
@@ -36,11 +36,33 @@ function checkResult(player, ai) {
     }
 }
 
+function publishResult(player, ai, result) {
+    document.querySelector('[data-sumary="your-choice"]').textContent = player;
+    document.querySelector('[data-sumary="ai-choice"]').textContent = ai;
+
+    document.querySelector('p.numbers span').textContent = ++gameSummary.numbers
+
+    if (result === "win") {
+        document.querySelector('p.wins span').textContent = ++gameSummary.wins
+        document.querySelector('[data-sumary="who-win"]').textContent = "Wygrałeś"
+        document.querySelector('[data-sumary="who-win"]').style.color = "#ffd700"
+    } else if (result === "losse") {
+        document.querySelector('p.losses span').textContent = ++gameSummary.losses
+        document.querySelector('[data-sumary="who-win"]').textContent = "Przegrałeś"
+        document.querySelector('[data-sumary="who-win"]').style.color = "red"
+    } else {
+        document.querySelector('p.draws span').textContent = ++gameSummary.draws
+        document.querySelector('[data-sumary="who-win"]').textContent = "Remis"
+        document.querySelector('[data-sumary="who-win"]').style.color = "black"
+    }
+}
+
 function startGame() {
     if (!game.playerHand) return alert("Wybierz głoń!");
     game.aiHand = aiChoice();
-    const gameResult = checkResult(game.playerHand, game.aiHand)
-    console.log(gameResult)
+    const gameResult = checkResult(game.playerHand, game.aiHand);
+    //console.log(gameResult)
+    publishResult(game.playerHand, game.aiHand, gameResult)
 }
 
 hands.forEach(hand => hand.addEventListener('click', handSelection))
