4ae63f1b72923ecf636b711468b23eb8dd78b27c
Konrad Pietkiewicz <konradpietkiewicz20@gmail.com>
2019-11-27 15:47:54 +0100

    Add  aiChoice and startGame

--- main.js
+++ main.js
@@ -19,4 +19,14 @@ function handSelection() {
     this.style.boxShadow = "0 0 0 3px blue"
 }
 
+function aiChoice() {
+    return hands[Math.floor(Math.random() * 3)].dataset.option;
+}
+
+function startGame() {
+    if (!game.playerHand) return alert("Wybierz głoń!")
+    game.aiHand = aiChoice()
+}
+
 hands.forEach(hand => hand.addEventListener('click', handSelection))
+document.querySelector('.start').addEventListener('click', startGame)
